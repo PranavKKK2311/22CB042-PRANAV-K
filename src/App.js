7e695ba1969487c88acea832f9149dc64fab2997
PranavKKK2311 <124026011+PranavKKK2311@users.noreply.github.com>
2024-11-08 14:19:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello world im Pranav The Goat 22CB042
+        Hello world im Pranav The GoaT 22CB042
         </p>
         <a
           className="App-link"
